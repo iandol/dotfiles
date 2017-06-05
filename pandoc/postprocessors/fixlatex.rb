30fb7c3dde4504085747169c24deeb05f12179d5
Ian <ian@diogenes.local>
2017-06-05 23:23:03 +0800

    remove debug code for filters

--- pandoc/postprocessors/fixlatex.rb
+++ pandoc/postprocessors/fixlatex.rb
@@ -1,18 +1,19 @@
 #!/usr/bin/env ruby
 #encoding: utf-8
+
 Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
 
 DEBUG = false
 if DEBUG
-	tstart = Time.now
-	PORT = 8991 
-	require 'byebug/core'
-	require 'byebug'
-	STDOUT.puts "\n!!!---DEBUG Server started on localhost:#{PORT} @ " + Time.now.to_s
-	Byebug.wait_connection = true
-	Byebug.start_server('localhost', PORT)
-	byebug
+  tstart = Time.now
+  PORT = 8991
+  require 'byebug/core'
+  require 'byebug'
+  STDOUT.puts "\n!!!---DEBUG Server started on localhost:#{PORT} @ " + Time.now.to_s
+  Byebug.wait_connection = true
+  Byebug.start_server('localhost', PORT)
+  byebug
 end
 
 input = $stdin.read
@@ -32,10 +33,10 @@ output.gsub!(/\bv3\b/i, 'V3')
 output.gsub!(/\bv4\b/i, 'V4')
 output.gsub!(/\\begin\{quote\}/, '\begin{quote}\emph{')
 output.gsub!(/\\end\{quote\}/, '}\end{quote}')
-output.gsub!(/\\label{references}/, '\label{references}\setstretch{0.8}')
+output.gsub!(/\\label{references}/, '\label{references}\setstretch{0.75}\sffamily\small')
 
 if DEBUG
-	tend = Time.now - tstart
-	output = output + " --- FIXLATEX PARSE TOOK: " + tend.to_s + " --- "
+  tend = Time.now - tstart
+  output = output + ' --- FIXLATEX PARSE TOOK: ' + tend.to_s + ' --- '
 end
 puts output
--- pandoc/preprocessors/removeAffiliations.rb
+++ pandoc/preprocessors/removeAffiliations.rb
@@ -1,25 +1,10 @@
 #!/usr/bin/env ruby
 #encoding: utf-8
+
 Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
 
-DEBUG = false
-if DEBUG
-	tstart = Time.now
-	PORT = 8991 
-	require 'byebug/core'
-	require 'byebug'
-	STDOUT.puts "\n!!!---DEBUG Server started on localhost:#{PORT} @ " + Time.now.to_s
-	Byebug.wait_connection = true
-	Byebug.start_server('localhost', PORT)
-	byebug
-end
-
 input = $stdin.read
-output = input.gsub(/(^\*\*Affiliations[^\n]+)/,"\n")
+output = input.gsub(/(^\*\*Affiliations[^\n]+)/, "\n")
 
-if DEBUG
-	tend = Time.now - tstart
-	output = output + "\n> REMOVEAFFILIATIONS TOOK: " + tend.to_s + "  \n\n"
-end
 puts output
