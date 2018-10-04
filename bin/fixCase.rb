b7e09b57274bf55f7774c2c814fc263b0bcc38b7
iandol <iandol@diogenes>
2018-10-04 09:44:41 +0200

    tweak fixCase.rb

--- bin/fixCase.rb
+++ bin/fixCase.rb
@@ -1,94 +1,96 @@
 #!/usr/bin/env ruby
 #encoding: utf-8
 
+# version = 1.0.0
+
 require 'tempfile'
 require 'fileutils'
 
 tstart = Time.now
 infilename = ARGV[0]
-fail "Please specify an existing file!" unless infilename and File.exist?(infilename)
+raise 'Please specify an existing file!' unless infilename && File.exist?(infilename)
 
 format = ARGV[1]
-if format.nil? || !(format =~ /(bib|json)/)
-	if infilename.match(/\.json$/)
-		format = 'json'
-	else
-		format = 'bib'
-	end
+if format.nil? || format !~ /(bib|json)/
+	infilename =~ /\.json$/ ? format = 'json' : format = 'bib'
 end
 
 print "===> Parsing #{format} Bibliography file: #{infilename} ..."
 
-#only wrap with {} IF the word is already the case in this list
+# only wrap with {} IF the word is already the case in this list
 keepCase = [
-	"ON",
-	"OFF",
-	"BETA",
-	"GAMMA",
-	"ALPHA",
-	"X", #X cells
-	"Y", #Y cells
-	"W", #W cells
-	"M", #magnocellular
-	"P", #parvocellular
+	'ON',
+	'OFF',
+	'BETA',
+	'GAMMA',
+	'ALPHA',
+	'X', # X cells
+	'Y', # Y cells
+	'W', # W cells
+	'M', # magnocellular
+	'P', # parvocellular
 ]
 
 # always force this word to be a certain case
 enforceCase = [
-	"X-cell",	"Y-cell",	"X-cells",	"Y-cells",
-	"V1",
-	"V2",
-	"V3",
-	"V4",
-	"V4d",
-	"V4v",
-	"V5",
-	"V6",
-	"V6a",
-	"MT",
-	"MST",
-	"hMT+",
-	"DNA",
-	"RNA",
-	"TRN",
-	"RTN",
-	"FEF",
-	"mPFC",
-	"VIP",
-	"mGlu1",
-	"GluR",
-	"NMDA",
-	"AMPA",
-	"GABA",
-	"GABAergic",
-	"LGN",
-	"dLGN",
-	"LFP",
-	"DREADD",
-	"ROC",
-	"fMRI",
-	"tDCS",
-	"EEG",
-	"VEP",
-	"AAV",
-	"TMS",
-	"MEG",
-	"2D",
-	"3D",
-	"HMAX",
-	"R-CNN",
-	"DCNN",
-	"NCC"]
+	'X-cell',
+	'Y-cell',
+	'X-cells',
+	'Y-cells',
+	'V1',
+	'V2',
+	'V3',
+	'V4',
+	'V4d',
+	'V4v',
+	'V5',
+	'V6',
+	'V6a',
+	'MT',
+	'MST',
+	'hMT+',
+	'DNA',
+	'RNA',
+	'TRN',
+	'RTN',
+	'FEF',
+	'mPFC',
+	'VIP',
+	'mGlu1',
+	'GluR',
+	'NMDA',
+	'AMPA',
+	'GABA',
+	'GABAergic',
+	'LGN',
+	'dLGN',
+	'LFP',
+	'DREADD',
+	'ROC',
+	'fMRI',
+	'tDCS',
+	'EEG',
+	'VEP',
+	'AAV',
+	'TMS',
+	'MEG',
+	'2D',
+	'3D',
+	'HMAX',
+	'R-CNN',
+	'DCNN',
+	'NCC'
+]
 
 temp_file = Tempfile.new('fixcase')
-case format 
+case format
 when /bib/
 	titleRegex = /^\s*title = /
 when /json/
 	titleRegex = /^\s*"title": /
 end
 
-#cusom boundarys, more specific than the generic \b 
+# cusom boundaries, more specific than the generic \b
 b1 = Regexp.escape("'\"/,.[(-–—")
 b2 = Regexp.escape("'\"/,.]):-–—")
 
@@ -97,38 +99,40 @@ begin
 		file.each_line do |line|
 			line.gsub!(/\r+$/, "\n")
 			if line.match(titleRegex)
-				keepCase.each do | k |
-					r = /(?<=[\s#{b1}])#{k}(?=[\s#{b2}])/ #case sensitive
+
+				keepCase.each do |k|
+					r = /(?<=[\s#{b1}])#{k}(?=[\s#{b2}])/ # case sensitive
 					case format
 					when /bib/
 						line.gsub!(r, "{#{k}}") if line.match(r)
-						sr = /title = {#{k}(?=\b)/ #deal with start-of-title words
-						er = /(?<=\b)#{k}},\s+\n/ #deal with end-of-title words
+						sr = /title = {#{k}(?=\b)/ # deal with start-of-title words
+						er = /(?<=\b)#{k}},\s+\n/ # deal with end-of-title words
 						line.gsub!(sr, "title = {{#{k}}") if line.match(sr)
 						line.gsub!(er, "{#{k}}}, \n") if line.match(er)
 					when /json/
 						if line.match(r)
-							line.gsub!(r, "<span class=\\\"nocase\\\">#{k}</span>") #case sensitive
+							line.gsub!(r, "<span class=\\\"nocase\\\">#{k}</span>") # case sensitive
 						end
 					end
-
-					rr = /#{k}},\s\n/
+					# rr = /#{k}},\s\n/
 				end
-				enforceCase.each do | e |
-					r = /(?<=[\s#{b1}])#{e}(?=[\s#{b2}])/i #case insensitive
+
+				enforceCase.each do |e|
+					r = /(?<=[\s#{b1}])#{e}(?=[\s#{b2}])/i # case insensitive
 					case format
 					when /bib/
 						line.gsub!(r, "{#{e}}") if line.match(r)
-						sr = /title = {#{e}(?=\b)/i #deal with start-of-title words
-						er = /(?<=\b)#{e}},\s+\n/i #deal with end-of-title words
+						sr = /title = {#{e}(?=\b)/i # deal with start-of-title words
+						er = /(?<=\b)#{e}},\s+\n/i # deal with end-of-title words
 						line.gsub!(sr, "title = {{#{e}}") if line.match(sr)
 						line.gsub!(er, "{#{e}}}, \n") if line.match(er)
 					when /json/
 						if line.match(r)
-							line.gsub!(r, "<span class=\\\"nocase\\\">#{e}</span>") #case sensitive
+							line.gsub!(r, "<span class=\\\"nocase\\\">#{e}</span>") # case sensitive
 						end
 					end
 				end
+
 			end
 			temp_file.puts line
 		end
@@ -141,4 +145,4 @@ ensure
 end
 
 tend = Time.now - tstart
-puts "... parsing took " + tend.to_s + "s"
+puts '... parsing took ' + tend.to_s + 's'
