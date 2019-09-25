1941608a9aaa71a9b32aa25d16b81e17686fc917
Ian Andolina <ian@Diogenes.local>
2019-09-25 10:21:44 +0200

    update ptb script to install newest kext

--- bin/ptbkernel.sh
+++ bin/ptbkernel.sh
@@ -1,7 +1,8 @@
 #!/bin/zsh
 cd /System/Library/Extensions
-echo "\n\nCheck directory:"
-sudo ls -al Psy*
+sudo mount -u -w / #need to remount as R/W
+echo "\n\List /System/Library/Extensions directory for Psy*:"
+ls -d Psy*
 echo "\n\nIs PsychtoolboxKernelDriver Currently Installed:"
 kextstat -b PsychtoolboxKernelDriver
 read -p "prompt"
@@ -9,11 +10,12 @@ echo "\n\nRemoving old driver, installing update..."
 sudo kextunload -b PsychtoolboxKernelDriver
 sleep 1
 sudo rm -rf PsychtoolboxKernelDriver.kext
-sudo unzip ~/Code/Psychtoolbox-3/Psychtoolbox/PsychHardware/PsychtoolboxKernelDriver64Bit.kext.zip
+sudo unzip ~/Code/Psychtoolbox/Psychtoolbox/PsychHardware/PsychtoolboxKernelDriverUpTodDate_Unsigned.kext.zip
 sleep 1
-echo "\n\nTrying to load new driver (this may fail on Yosemite, will need to change boot options)"
+echo "\n\nTrying to load new driver (this may fail, if so disable SIP)"
+sleep 1
 sudo kextload -b PsychtoolboxKernelDriver
 kextstat -b PsychtoolboxKernelDriver
 read -p "prompt"
 #echo "\n\nSetting kext override loading for Yosemite+ (boot-args='kext-dev-mode=1'); reboot if this is the first time you do this"
-#sudo nvram boot-args="-v"
+#ma
