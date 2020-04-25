c596207b102ad5fb73cfbe4be4f3189848b59ab7
Iandol <ian@diogenes>
2020-04-26 07:59:34 +0800

    small update

--- bin/ubuntu-mainline-kernel.sh
+++ bin/ubuntu-mainline-kernel.sh
@@ -1,24 +1,14 @@
 #!/usr/bin/env bash
 
-# Check if we are running on an Ubuntu-like OS
-[ -f "/etc/os-release" ] && {
-    source /etc/os-release
-    [[ "$ID" == "ubuntu" ]] || [[ "$ID_LIKE" =~ "ubuntu" ]]
-} || {
-    OS=$(lsb_release -si 2>&-)
-    [[ "$OS" == "Ubuntu" ]] || [[ "$OS" == "LinuxMint" ]]  || [[ "$OS" == "neon" ]] || { 
-        echo "Abort, this script is only intended for Ubuntu-like distro's"
-        exit 2
-    }
-}
+# shellcheck disable=SC1117
 
 # Ubuntu Kernel PPA info
 ppa_host="kernel.ubuntu.com"
 ppa_index="/~kernel-ppa/mainline/"
 ppa_key="17C622B0"
 
-# If quite=1 then no log messages are printed (except errors)
-quite=0
+# If quiet=1 then no log messages are printed (except errors)
+quiet=0
 
 # If check_signature=0 then the signature of the CHECKSUMS file will not be checked
 check_signature=1
@@ -27,22 +17,22 @@ check_signature=1
 check_checksum=1
 
 # If doublecheckversion=1 then also check the version specific ppa page to make
-# sure the kernel build was succesfull
+# sure the kernel build was successful
 doublecheckversion=1
 
 # Connect over http or https to ppa (only https works)
 use_https=1
 
-# Path to sudo command
-sudo=$(command -v sudo)
+# Path to sudo command, empty by default
+sudo=""
+#sudo=$(command -v sudo) # Uncomment this line if you dont want to sudo yourself
 
 # Path to wget command
 wget=$(command -v wget)
 
-
 #####
 ## Below are internal variables of which most can be toggled by command options
-## DONT CHANGE THESE MANUALLY
+## DON'T CHANGE THESE MANUALLY
 #####
 
 # (internal) If cleanup_files=1 then before exiting all downloaded/temporaryfiles
@@ -89,7 +79,7 @@ REMOTE_VERSIONS=()
 # (internal) The architecture of the local system
 arch=$(dpkg --print-architecture)
 
-# (internal) The text to search for to check if the build was succesful
+# (internal) The text to search for to check if the build was successfully
 build_succeeded_text="Build for ${arch} succeeded"
 
 # (internal) The pid of the child process which checks download progress
@@ -99,6 +89,22 @@ monitor_pid=0
 download_size=0
 
 action_data=()
+
+#####
+## Check if we are running on an Ubuntu-like OS
+#####
+
+# shellcheck disable=SC1091,SC2015
+[ -f "/etc/os-release" ] && {
+    source /etc/os-release
+    [[ "$ID" == "ubuntu" ]] || [[ "$ID_LIKE" =~ "ubuntu" ]]
+} || {
+    OS=$(lsb_release -si 2>&-)
+    [[ "$OS" == "Ubuntu" ]] || [[ "$OS" == "LinuxMint" ]]  || [[ "$OS" == "neon" ]] || {
+        echo "Abort, this script is only intended for Ubuntu-like distro's"
+        exit 2
+    }
+}
 
 #####
 ## helper functions
@@ -112,15 +118,15 @@ single_action () {
 }
 
 log () {
-    [ $quite -eq 0 ] && echo "$@"
+    [ $quiet -eq 0 ] && echo "$@"
 }
 
 logn () {
-    [ $quite -eq 0 ] && echo -n "$@"
+    [ $quiet -eq 0 ] && echo -n "$@"
 }
 
 warn () {
-    [ $quite -eq 0 ] && echo "$@" >&2
+    [ $quiet -eq 0 ] && echo "$@" >&2
 }
 
 err () {
@@ -133,7 +139,7 @@ err () {
 
 while (( "$#" )); do
     argarg_required=0
-    
+
     case $1 in
         -c|--check)
             single_action
@@ -164,43 +170,43 @@ while (( "$#" )); do
                 err "Option $1 requires an argument."
                 exit 2
             else
-                workdir="$2"
+                workdir="$(realpath "$2")/"
                 shift
-                
+
                 if [ ! -d "$workdir" ]; then
                     mkdir -p "$workdir";
                 fi
-                
-                if [ ! -d "$workdir" ] || [ ! -w "$workdir" ]; then    
+
+                if [ ! -d "$workdir" ] || [ ! -w "$workdir" ]; then
                     err "$workdir is not writable"
                     exit 1
                 fi
-                
+
                 cleanup_files=0
             fi
             ;;
         -ll|--lowlatency|--low-latency)
-            [[ "$arch" != "amd64" ]] && [[ "$arch" != "i386" ]] && { 
+            [[ "$arch" != "amd64" ]] && [[ "$arch" != "i386" ]] && {
                 err "Low-latency kernels are only available for amd64 or i386 architectures"
                 exit 3
             }
-            
+
             use_lowlatency=1
             ;;
         -lpae|--lpae)
-            [[ "$arch" != "armhf" ]] && { 
+            [[ "$arch" != "armhf" ]] && {
                 err "Large Physical Address Extension (LPAE) kernels are only available for the armhf architecture"
                 exit 3
             }
-            
+
             use_lpae=1
             ;;
         --snapdragon)
-            [[ "$arch" != "arm64" ]] && { 
+            [[ "$arch" != "arm64" ]] && {
                 err "Snapdragon kernels are only available for the arm64 architecture"
                 exit 3
             }
-            
+
             use_snapdragon=1
             ;;
         --rc)
@@ -212,8 +218,8 @@ while (( "$#" )); do
         --yes)
             assume_yes=1
             ;;
-        -q|--quite)
-            [ "$debug_target" == "/dev/null" ] && { quite=1; }
+        -q|--quiet)
+            [ "$debug_target" == "/dev/null" ] && { quiet=1; }
             ;;
         -do|--download-only)
             do_install=0
@@ -227,7 +233,7 @@ while (( "$#" )); do
             ;;
         -d|--debug)
             debug_target="/dev/stderr"
-            quite=0
+            quiet=0
             ;;
         -h|--help)
             run_action="help"
@@ -237,14 +243,15 @@ while (( "$#" )); do
             err "Unknown argument $1"
             ;;
     esac
-    
+
     if [ $argarg_required -eq 1 ]; then
         [ -n "$2" ] && [ "${2##-}" == "$2" ] && {
             action_data+=("$2")
             shift
         }
     elif [ $argarg_required -eq 2 ]; then
-        [ -n "$2" ] && [ "${2##-}" == "$2" ] && { 
+        # shellcheck disable=SC2015
+        [ -n "$2" ] && [ "${2##-}" == "$2" ] && {
             action_data+=("$2")
             shift
         } || {
@@ -252,7 +259,7 @@ while (( "$#" )); do
             exit 2
         }
     fi
-    
+
     shift
 done
 
@@ -287,6 +294,7 @@ monitor_progress () {
     printf "%s: " "$msg"
     (while :; do for c in / - \\ \|; do
         [[ -f "$file" ]] && {
+            # shellcheck disable=SC2015
             [[ $download_size -le 0 ]] && {
                 download_size=$(($(head -n20 "$file" | grep -aoi -E "Content-Length: [0-9]+" | cut -d" " -f2) + 0))
                 printf ' %d%% %s' 0 "$c"
@@ -321,7 +329,7 @@ remove_http_headers () {
             mv "${file}.tmp" "${file}"
             break
         fi
-        
+
         [ $nr -gt 100 ] && {
             err "Abort, could not remove http headers from file"
             exit 3
@@ -350,9 +358,7 @@ latest_local_version() {
 
     if [ ${#LOCAL_VERSIONS[@]} -gt 0 ]; then
         local sorted
-        IFS=$'\n'
-        sorted=($(sort -t"." -k1V,3 <<<"${LOCAL_VERSIONS[*]}"))
-        unset IFS
+        mapfile -t sorted < <(echo "${LOCAL_VERSIONS[*]}" | tr ' ' '\n' | sort -t"." -k1V,3)
 
         lv="${sorted[${#sorted[@]}-1]}"
         echo "${lv/-[0-9][0-9][0-9][0-9][0-9][0-9]rc/-rc}"
@@ -364,7 +370,7 @@ latest_local_version() {
 remote_html_cache=""
 load_remote_versions () {
     local line
-    
+
     [[ -n "$2" ]] && {
       REMOTE_VERSIONS=()
     }
@@ -397,7 +403,7 @@ latest_remote_version () {
     load_remote_versions 1 "$1"
     local sorted
 
-    sorted=($(echo ${REMOTE_VERSIONS[*]} | tr ' ' '\n' | sort -V | tr '\n' ' '))
+    mapfile -t sorted < <(echo "${REMOTE_VERSIONS[*]}" | tr ' ' '\n' | sort -V)
     echo "${sorted[${#sorted[@]}-1]}"
 }
 
@@ -417,10 +423,10 @@ Download & install the latest kernel available from $ppa_host$ppa_uri
 
 Arguments:
   -c               Check if a newer kernel version is available
-  -i [VERSION]     Install kernel VERSION, see -l for list. You dont have to prefix
+  -i [VERSION]     Install kernel VERSION, see -l for list. You don't have to prefix
                    with v. E.g. -i 4.9 is the same as -i v4.9. If version is
                    omitted the latest available version will be installed
-  -l [SEarch]      List locally installedkernel versions. If an argument to this
+  -l [SEarch]      List locally installed kernel versions. If an argument to this
                    option is supplied it will search for that
   -r [SEarch]      List available kernel versions. If an argument to this option
                    is supplied it will search for that
@@ -430,7 +436,7 @@ Arguments:
 
 Optional:
   -s, --signed         Only install signed kernel packages (not implemented)
-  -p, --path DIR       The working directory, .deb files will be downloaded into 
+  -p, --path DIR       The working directory, .deb files will be downloaded into
                        this folder. If omitted, the folder /tmp/$(basename "$0")/
                        is used. Path is relative from \$PWD
   -ll, --low-latency   Use the low-latency version of the kernel, only for amd64 & i386
@@ -452,13 +458,13 @@ Optional:
         logn "Finding latest version available on $ppa_host"
         latest_version=$(latest_remote_version)
         log ": $latest_version"
-        
+
         logn "Finding latest installed version"
         installed_version=$(latest_local_version)
         installed_version=${installed_version%-*}
         log ": $installed_version"
 
-        # Check if build was successfull
+        # Check if build was successful
         if [ $doublecheckversion -gt 0 ]; then
             ppa_uri=$ppa_index${latest_version%\.0}"/"
             ppa_uri=${ppa_uri/\.0-rc/-rc}
@@ -466,8 +472,8 @@ Optional:
             index=$(download $ppa_host "$ppa_uri")
             if [[ ! $index =~ $build_succeeded_text ]]; then
                  log "A newer kernel version ($latest_version) was found but the build was not successful"
-                
-                [ -x "$(command -v notify-send)" ] && notify-send --icon=info -t 12000 \
+
+                [ -n "$DISPLAY" ] && [ -x "$(command -v notify-send)" ] && notify-send --icon=info -t 12000 \
                     "Kernel $latest_version available" \
                     "A newer kernel version ($latest_version) is\navailable but the build was not successful"
                 exit 1
@@ -489,8 +495,8 @@ Optional:
 
         if [ "$installed_version" != "$latest_version" ] && [ "$installed_version" = "$(echo -e "$latest_version\n$installed_version" | sort -V | head -n1)" ]; then
             log "A newer kernel version ($latest_version) is available${latest_minor_text}"
-            
-            [ -x "$(command -v notify-send)" ] && notify-send --icon=info -t 12000 \
+
+            [ -n "$DISPLAY" ] && [ -x "$(command -v notify-send)" ] && notify-send --icon=info -t 12000 \
                 "Kernel $latest_version available" \
                 "A newer kernel version ($latest_version) is available\n\n${latest_minor_notify}Run '$(basename "$0") -i' to update\nor visit $ppa_host$ppa_uri"
             exit 1
@@ -498,9 +504,10 @@ Optional:
         ;;
     local-list)
         load_local_versions
-        
+
+        # shellcheck disable=SC2015
         [[ -n "$(command -v column)" ]] && { column="column -x"; } || { column="cat"; }
-        
+
         (for version in "${LOCAL_VERSIONS[@]}"; do
             if [ -z "${action_data[0]}" ] || [[ "$version" =~ ${action_data[0]} ]]; then
                 echo "$version"
@@ -510,9 +517,10 @@ Optional:
     remote-list)
         check_environment
         load_remote_versions
-        
+
+        # shellcheck disable=SC2015
         [[ -n "$(command -v column)" ]] && { column="column -x"; } || { column="cat"; }
-        
+
         (for version in "${REMOTE_VERSIONS[@]}"; do
             if [ -z "${action_data[0]}" ] || [[ "$version" =~ ${action_data[0]} ]]; then
                 echo "$version"
@@ -522,63 +530,63 @@ Optional:
     install)
         check_environment
         load_local_versions
-        
+
         if [ -z "${action_data[0]}" ]; then
             logn "Finding latest version available on $ppa_host"
             version=$(latest_remote_version)
             log
-            
+
             if containsElement "$version" "${LOCAL_VERSIONS[@]}"; then
                 logn "Latest version is $version but seems its already installed"
             else
                 logn "Latest version is: $version"
             fi
-                
+
             if [ $do_install -gt 0 ] && [ $assume_yes -eq 0 ];then
                 logn ", continue? (y/N) "
-                [ $quite -eq 0 ] && read -rsn1 continue
+                [ $quiet -eq 0 ] && read -rsn1 continue
                 log
-                
+
                 [ "$continue" != "y" ] && [ "$continue" != "Y" ] && { exit 0; }
             else
                 log
             fi
         else
             load_remote_versions
-            
+
             version=""
             if containsElement "v${action_data[0]#v}" "${REMOTE_VERSIONS[@]}"; then
                 version="v"${action_data[0]#v}
             fi
-            
+
             [[ -z "$version" ]] && {
                 err "Version '${action_data[0]}' not found"
                 exit 2
             }
             shift
-            
+
             if [ $do_install -gt 0 ] && containsElement "$version" "${LOCAL_VERSIONS[@]}" && [ $assume_yes -eq 0 ]; then
                 logn "It seems version $version is already installed, continue? (y/N) "
-                [ $quite -eq 0 ] && read -rsn1 continue
+                [ $quiet -eq 0 ] && read -rsn1 continue
                 log
-                
+
                 [ "$continue" != "y" ] && [ "$continue" != "Y" ] && { exit 0; }
             fi
         fi
-        
-        [ ! -d "$workdir" ] && { 
+
+        [ ! -d "$workdir" ] && {
             mkdir -p "$workdir" 2>/dev/null
         }
         [ ! -x "$workdir" ] && {
             err "$workdir is not writable"
             exit 1
         }
-        
+
         cd "$workdir" || exit 1
-        
+
         [ $check_signature -eq 1 ] && [ -z "$(command -v gpg)" ] && {
             check_signature=0
-            
+
             warn "Disable signature check, gpg not available"
         }
 
@@ -603,7 +611,7 @@ Optional:
         index=${index##*<table}
         for line in $index; do
             [[ "$line" =~ linux-(image(-(un)?signed)?|headers|modules)-[0-9]+\.[0-9]+\.[0-9]+-[0-9]{6}.*?_(${arch}|all).deb ]] || continue
-            
+
             [ $use_lowlatency -eq 0 ] && [[ "$line" =~ "-lowlatency" ]] && continue
             [ $use_lowlatency -eq 1 ] && [[ ! "$line" =~ "-lowlatency" ]] && [[ ! "$line" =~ "_all" ]] && continue
             [ $use_lpae -eq 0 ] && [[ "$line" =~ "-lpae" ]] && continue
@@ -635,10 +643,10 @@ Optional:
         for file in "${FILES[@]}"; do
             monitor_progress "Downloading $file" "$workdir$file"
             download $ppa_host "$ppa_uri$file" > "$workdir$file"
-            
+
             remove_http_headers "$workdir$file"
             end_monitor_progress
-            
+
             if [[ "$file" =~ \.deb ]]; then
                 debs+=("$file")
             fi
@@ -647,7 +655,7 @@ Optional:
         if [ $check_signature -eq 1 ]; then
             if ! gpg --list-keys ${ppa_key} >$debug_target 2>&1; then
                 logn "Importing kernel-ppa gpg key "
-                
+
                 if gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv ${ppa_key} >$debug_target 2>&1; then
                     log "ok"
                 else
@@ -659,7 +667,7 @@ Optional:
 
             if [ $check_signature -eq 1 ]; then
                 if gpg --verify CHECKSUMS.gpg CHECKSUMS >$debug_target 2>&1; then
-                    log "Signature of checksum file has been succesfully verified"
+                    log "Signature of checksum file has been successfully verified"
                 else
                     err "Abort, signature of checksum file is NOT OK"
                     exit 4
@@ -669,17 +677,18 @@ Optional:
 
         if [ $check_checksum -eq 1 ]; then
             shasums=( "sha256sum" "sha1sum" )
-            
+
             for shasum in "${shasums[@]}"; do
                 xshasum=$(command -v "$shasum")
                 if [ -n "$xshasum" ] && [ -x "$xshasum" ]; then
+                    # shellcheck disable=SC2094
                     shasum_result=$($xshasum --ignore-missing -c CHECKSUMS 2>>$debug_target | tee -a $debug_target | wc -l)
 
                     if [ "$shasum_result" -eq 0 ] || [ "$shasum_result" -ne ${#debs[@]} ]; then
-                        err "Abort, $shasum retuned an error $shasum_result"
+                        err "Abort, $shasum returned an error $shasum_result"
                         exit 4
                     else
-                        log "Checksums of deb files have been succesfully verified with $shasum"
+                        log "Checksums of deb files have been successfully verified with $shasum"
                     fi
 
                     break
@@ -717,14 +726,14 @@ Optional:
             for version in "${LOCAL_VERSIONS[@]}"; do
                 echo "[$nr]: $version"
                 nr=$((nr + 1))
-                
+
                 [ $nr -gt 9 ] && break
             done
-            
+
             echo -n "type the number between []: "
             read -rn1 index
             echo ""
-            
+
             uninstall_version=${LOCAL_VERSIONS[$index]}
         elif containsElement "v${action_data[0]#v}" "${LOCAL_VERSIONS[@]}"; then
             uninstall_version="v"${action_data[0]#v}
@@ -740,10 +749,10 @@ Optional:
         else
             continue="y"
         fi
-        
+
         if [ "$continue" == "y" ] || [ "$continue" == "Y" ]; then
             IFS=$'\n'
-            
+
             pckgs=()
             for pckg in $(dpkg -l linux-{image,image-[un]?signed,headers,modules}-"${uninstall_version#v}"* 2>$debug_target | cut -d " " -f 3); do
                 # only match kernels from ppa, they have 6 characters as second version string
@@ -753,26 +762,26 @@ Optional:
                 elif [[ "$pckg" =~ linux-(image(-(un)?signed)?|modules)-[0-9]+\.[0-9]+\.[0-9]+-[0-9]{6} ]]; then
                     pckgs+=("$pckg:$arch")
                 fi
-            done    
-            
+            done
+
             if [ ${#pckgs[@]} -eq 0 ]; then
                 warn "Did not find any packages to remove"
             else
                 echo "The following packages will be removed: "
                 echo "${pckgs[@]}"
-                
+
                 if [ $assume_yes -eq 0 ]; then
                     echo -n "Are you really sure? Do you still have another kernel installed? (y/N)"
-                    
+
                     read -rsn1 continue
                     echo ""
                 else
                     continue="y"
                 fi
-                
+
                 if [ "$continue" == "y" ] || [ "$continue" == "Y" ]; then
                     if $sudo DEBIAN_FRONTEND=noninteractive dpkg --purge "${pckgs[@]}" 2>$debug_target >&2; then
-                        log "Kernel $uninstall_version succesfully purged"
+                        log "Kernel $uninstall_version successfully purged"
                         exit 0
                     fi
                 fi
