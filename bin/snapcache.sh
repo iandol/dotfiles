351f5f3241513037d6ba2147b5e6baa39c85aa9a
iandol <iandol@seneca.org>
2024-05-20 13:23:58 +0800

    tweaks

--- bin/snapcache.sh
+++ bin/snapcache.sh
@@ -2,7 +2,6 @@
 # Removes old revisions of snaps
 # CLOSE ALL SNAPS BEFORE RUNNING THIS
 set -eu
-snap list --all | awk '/disabled/{print $1, $3}' |
-    while read snapname revision; do
-        snap remove "$snapname" --revision="$revision"
-    done
+snap list --all | awk '/disabled/{print $1, $3}' | while read snapname revision; do 
+	snap remove "$snapname" --revision="$revision" 
+done
