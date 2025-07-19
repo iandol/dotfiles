863e9885a676e37e3b90808d73a947fdf5af7a46
iandol <iandol@seneca.org>
2025-07-19 18:19:30 +0800

    nvim tweaks

--- configs/init.lua
+++ configs/init.lua
@@ -15,13 +15,46 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Set some basic options
-vim.cmd("set encoding=utf-8")
-vim.cmd("set fileencoding=utf-8")
-vim.cmd("set noexpandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
+-- Basic settings
+vim.opt.number = true							-- Line numbers
+vim.opt.relativenumber = true					-- Relative line numbers
+vim.opt.cursorline = true						-- Highlight current line
+vim.opt.wrap = false							-- Don't wrap lines
+vim.opt.scrolloff = 10							-- Keep 10 lines above/below cursor 
+vim.opt.sidescrolloff = 8						-- Keep 8 columns left/right of cursor
+
+vim.o.fileencoding = 'utf-8' -- the encoding written to a file
+
+vim.opt.smartindent = true						-- Smart auto-indenting
+vim.opt.autoindent = true						-- Copy indent from current line
+
+-- Search settings
+vim.opt.ignorecase = true						-- Case insensitive search
+vim.opt.smartcase = true						-- Case sensitive if uppercase in search
+vim.opt.hlsearch = false						-- Don't highlight search results 
+vim.opt.incsearch = true						-- Show matches as you type
+
+-- Visual settings
+vim.opt.termguicolors = true					-- Enable 24-bit colors
+vim.opt.signcolumn = "yes"						-- Always show sign column
+vim.opt.colorcolumn = "100"						-- Show column at 100 characters
+vim.opt.showmatch = true									-- Highlight matching brackets
+
+-- Split behavior
+vim.opt.splitbelow = true						-- Horizontal splits go below
+vim.opt.splitright = true						-- Vertical splits go right
+
+vim.wo.number = true							-- Make line numbers default
+vim.o.mouse = 'a'								-- Enable mouse mode
+vim.o.clipboard = 'unnamedplus'					-- Sync clipboard between OS and Neovim.
+vim.o.breakindent = true						-- Enable break indent
+vim.o.undofile = true							-- Save undo history
+
+vim.o.smartcase = true							-- smart case
+vim.o.shiftwidth = 4							-- the number of spaces inserted for each indentation
+vim.o.tabstop = 4								-- insert n spaces for a tab
+vim.o.softtabstop = 4							-- Number of spaces that a tab counts for while performing editing operations
+vim.o.expandtab = false							-- convert tabs to spaces
 
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
@@ -58,14 +91,8 @@ require("lazy").setup({
 				"nvim-lua/plenary.nvim",
 				"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 				"MunifTanjim/nui.nvim",
-				-- Optional image support for file preview: See `# Preview Mode` for more information.
-				-- {"3rd/image.nvim", opts = {}},
-				-- OR use snacks.nvim's image module:
-				-- "folke/snacks.nvim",
+				"folke/snacks.nvim",
 			},
-			lazy = false, -- neo-tree will lazily load itself
-			---@module "neo-tree"
-			---@type neotree.Config?
 			opts = {
 				window = { position = "right", },
 			},
@@ -73,21 +100,17 @@ require("lazy").setup({
 		{
 			"folke/which-key.nvim",
 			event = "VeryLazy",
-			opts = {
-				-- your configuration comes here
-				-- or leave it empty to use the default settings
-				-- refer to the configuration section below
-			},
+			opts = {},
 			keys = {
 				{
-				"<leader>wk",
+				"<leader>?",
 				function()
-					require("which-key").show({ global = false })
+					require("which-key").show({ global = true })
 				end,
 				desc = "Buffer Local Keymaps (which-key)",
 				},
 			},
-		}
+		},
 	},
 	-- Configure any other settings here. See the documentation for more details.
 	-- colorscheme that will be used when installing plugins.
@@ -100,6 +123,10 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find f
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<leader>n', ':Neotree<cr>', { desc = 'Open Neotree' })
+
+vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal right<CR>", {})
+vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
 
 local configs = require("nvim-treesitter.configs")
 configs.setup({
