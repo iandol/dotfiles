9c5bb150bed048d3214b2322d56ad7d7c95b7067
iandol <iandol@cog5.org>
2025-09-03 21:15:28 +0800

    sshfs (only for linux)

--- tools/yazi/init.lua
+++ tools/yazi/init.lua
@@ -1,1 +1,10 @@
 require("starship"):setup()
+
+-- Minimal reliable setup
+require("sshfs"):setup({
+  sshfs_options = {
+    "reconnect",
+    "ServerAliveInterval=15",
+    "ServerAliveCountMax=3",
+  },
+})
