cbc50d9639a6d7ea2386c189c5c687b26138335b
iandol <iandol@seneca.org>
2025-09-16 21:32:14 +0800

    tweaks

--- editors/nvim/lua/plugins/which-key.lua
+++ editors/nvim/lua/plugins/which-key.lua
@@ -7,5 +7,36 @@ return {
 		preset = "modern",
 		notify = true,
 	},
-	keys = {{"<leader>?", function() require("which-key").show({ global = true }) end, desc = "Buffer Local Keymaps (which-key)"}},
+	---@type wk.Win.opts
+	win = {
+		padding = { 1, 1},
+	},
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").show({ global = true })
+			end,
+			desc = "Buffer Local Keymaps (which-key)"
+		}
+	},
+	plugins = {
+		marks = true, -- shows a list of your marks on ' and `
+		registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+		-- the presets plugin, adds help for a bunch of default keybindings in Neovim
+		-- No actual key bindings are created
+		spelling = {
+			enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+			suggestions = 20, -- how many suggestions should be shown in the list?
+		},
+		presets = {
+			operators = true, -- adds help for operators like d, y, ...
+			motions = true, -- adds help for motions
+			text_objects = true, -- help for text objects triggered after entering an operator
+			windows = true, -- default bindings on <c-w>
+			nav = true, -- misc bindings to work with windows
+			z = true, -- bindings for folds, spelling and others prefixed with z
+			g = true, -- bindings for prefixed with g
+	    },
+	  },
 }
