d4da71edf2cda6bc4663d3d29e2a78d0f88957da
iandol <iandol@cog5.org>
2025-11-05 09:56:49 +0800

    eget updates

---
+++ bin/egetAll.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+eget zyedidia/eget --to=usr/local/bin
+eget volllly/rotz --to=/usr/local/bin -a gnu.zip
+eget bluenviron/mediamtx --to=/usr/local/bin
+eget clash-verge-rev/clash-verge-rev -a amd64.deb --to=~/Downloads
+eget --pre-release sxyazi/yazi --to=$E:HOME/bin --file="
+yazi" --file="ya" -a gnu.zip
+eget --pre-release sxyazi/yazi --to=$E:HOME/bin --file="
+yazi" --file="yazi" -a gnu.zip
+eget MetaCubeX/mihomo --to=/usr/local/bin -a gz -a amd64
+-v3-v
+eget SamuNatsu/mihomosh --to=/usr/local/bin -a gz
+
