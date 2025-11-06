9ee0fe22e9648e97daab1d00004baf348306ba5a
iandol <iandol@cog5.org>
2025-11-06 11:07:25 +0800

    fix

--- bin/egetAll.sh
+++ bin/egetAll.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env zsh
 
-eget zyedidia/eget --to=usr/local/bin
-eget volllly/rotz --to=/usr/local/bin -a gnu.zip
+eget zyedidia/eget --upgrade-only --to=/usr/local/bin
+eget volllly/rotz --upgrade-only ---to=/usr/local/bin -a gnu.zip
 eget bluenviron/mediamtx --to=/usr/local/bin
 eget clash-verge-rev/clash-verge-rev -a amd64.deb --to=~/Downloads/clash.deb
-eget --pre-release sxyazi/yazi --to=$HOME/bin --file="
-yazi" --file="ya" -a gnu.zip
-eget --pre-release sxyazi/yazi --to=$HOME/bin --file="
-yazi" --file="yazi" -a gnu.zip
-eget MetaCubeX/mihomo --to=/usr/local/bin -a gz -a amd64-v3-v
-eget SamuNatsu/mihomosh --to=/usr/local/bin -a gz
+eget --pre-release sxyazi/yazi --to=$HOME/bin --file="ya" -a gnu.zip
+eget --pre-release sxyazi/yazi --to=$HOME/bin --file="yazi" -a gnu.zip 
+eget MetaCubeX/mihomo --upgrade-only ---to=/usr/local/bin -a gz -a amd64-v3-v
+eget SamuNatsu/mihomosh --upgrade-only ---to=/usr/local/bin -a gz
 
