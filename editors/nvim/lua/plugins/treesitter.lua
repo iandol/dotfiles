c674d0e610bcc17ee6192e0ffd823c0a0c6e332b
iandol <iandol@cog5.org>
2026-06-05 16:54:17 +0800

    tresitter update

--- editors/nvim/lua/plugins/treesitter.lua
+++ editors/nvim/lua/plugins/treesitter.lua
@@ -1,8 +1,9 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	lazy = "false",
 	build = ":TSUpdate",
 	config = function()
-		require("nvim-treesitter.configs").setup({
+		require("nvim-treesitter").setup({
 			ensure_installed = { "ruby", "python", "matlab",
 				"lua", "vim", "vimdoc", "elvish", "bash", "ini",
 				"json", "yaml", "toml", "xml", "javascript", "html" },
